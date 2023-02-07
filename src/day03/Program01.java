5a0e8b5f6107d37b685b3f29208e67a7384b801d
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-07 17:46:32 +1100

    Day03
    java syntax
    main method

--- src/day03/Program01.java
+++ src/day03/Program01.java
@@ -3,8 +3,21 @@ package day03;
 public class Program01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("good");
+		System.out.println("day");
+		fun();
+		fun();
+		fun();
+		
 	}
+public static void fun() {
+	System.out.println("good morning...!");
+	
+	}
+		
+	
+	
+
 
 }
