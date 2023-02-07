5799c865ee543352298fd42a67eae1ad741564cb
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-07 18:53:23 +1100

    day03
    java syntax
    main method
    fun1 method
    sun_ method

--- src/day03/Program02.java
+++ src/day03/Program02.java
@@ -3,8 +3,24 @@ package day03;
 public class Program02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		System.out.println("Hello World...!");
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("Happy");
+		System.out.println("Faith");
+		System.out.println();
+fun1();
+sun_();
 	}
+public static void fun1() {
+	System.out.println("Faith../");
+	System.out.println("Hello World...");
+	System.out.println("345ertyu");
+
+}
+public static void sun_() {
+	System.out.println("Run..");
+}
 
 }
