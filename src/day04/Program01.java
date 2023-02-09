c3efcc5de02185b3462323f7bb5c1419b5f8ef00
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-09 12:17:34 +1100

    Day04
    writing a method for different values of int.

---
+++ src/day04/Program01.java
@@ -0,0 +1,30 @@
+package day04;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		
+cal(10,20);
+cal(12,13);
+cal(14,16);
+cal(17,18);
+
+	}
+	public static void cal(int a, int b) {
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		
+		
+		
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+}
