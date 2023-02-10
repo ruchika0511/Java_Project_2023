2e34a2aee017d2b3d1083fc758fca285a35f5c76
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-10 12:55:04 +1100

    Day04
    creating an object for non static method

---
+++ src/day04/Program02.java
@@ -0,0 +1,30 @@
+package day04;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+	Program02 xyz=	new Program02();	
+			xyz.addMethod(23,30);
+			xyz.subMethod(23,30);
+			xyz.mulMethod(23,30);
+
+	}
+public void addMethod(int a, int b) {
+	
+	System.out.println(a+b);
+}
+public void subMethod(int a,int b) {
+	System.out.println(a-b);
+}
+public void mulMethod(int a, int b) {
+	System.out. println(a*b);
+}
+
+
+
+
+
+
+
+
+}
