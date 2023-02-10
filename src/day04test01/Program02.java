5dec3497f7d4133d9f171d7acea1ffc20102df63
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-10 13:30:05 +1100

    Day04
    extending program from one class to another

---
+++ src/day04test01/Program02.java
@@ -0,0 +1,12 @@
+package day04test01;
+
+public class Program02 extends Program01{
+
+	public static void main(String[] args) {
+	addMethod(12,13);
+	subMethod(10,15);
+	mulMethod(20,25);
+
+	}
+
+}
