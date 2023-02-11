7d14e3f6eca44bb1540911e2162e701551910d75
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-11 16:42:05 +1100

    Day04
    extending program from one class to another , from non static to static method by creating new object

--- src/day04test01/Program01.java
+++ src/day04test01/Program01.java
@@ -1,14 +1,14 @@
 package day04test01;
 
 public class Program01 {
-	public static void addMethod(int a, int b) {
+	public void addMethod(int a, int b) {
 		
 		System.out.println(a+b);
 	}
-	public static void subMethod(int a,int b) {
+	public void subMethod(int a,int b) {
 		System.out.println(a-b);
 	}
-	public static void mulMethod(int a, int b) {
+	public void mulMethod(int a, int b) {
 		System.out. println(a*b);
 	}
 
