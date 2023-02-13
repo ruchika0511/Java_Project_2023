5ef7cb165c351d6a2d6290669f9263a63a3958e0
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-14 00:10:23 +1100

    Day08
    control Statements- 'if' condition

---
+++ src/day08/Program01.java
@@ -0,0 +1,19 @@
+package day08;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		int a=15;
+		int b=20;
+		
+	
+		
+	    if(a>b)
+	    	if(a<b)
+		
+		{
+			System.out.println("Good Afternoon...");
+			System.out.println("Good Evenining...");
+		}
+}
+}
