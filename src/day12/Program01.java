c0380e70bcc6d382c123485b56d5125168e19411
ruchika0511 <123642647+ruchika0511@users.noreply.github.com>
2023-02-27 12:37:45 +1100

    day12
    post increment
    pre increment

---
+++ src/day12/Program01.java
@@ -0,0 +1,17 @@
+package day12;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		int a=1;
+		//a++  ++a  -----> a=a+1
+		System.out.println(a);
+		int b=a++;
+		System.out.println(b+"   "+a);
+		int c=++a;
+		System.out.println(c+"   "+a);
+		
+
+	}
+
+}
